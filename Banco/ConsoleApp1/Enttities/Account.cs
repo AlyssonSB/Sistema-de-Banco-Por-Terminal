1b00d3565c7251ec457eef46217f9d74db34ba8f
AlyssonSB <alysson.serafim132@gmail.com>
2025-04-29 09:25:09 -0300

    Inicio do Projeto

---
+++ Banco/ConsoleApp1/Enttities/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course;
+
+namespace Course.Entities
+
+{
+    class Account
+    {
+        public Account() { }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Number { get; set; }
+        public double Balance { get; set; }
+        public string Address { get; set; }
+
+        public Account(string name, string email, string address, int number)
+        {
+            Name = name;
+            Email = email;
+            Address = address; 
+            Number = number; 
+            Balance = 0;
+        }
+
+        public string Grande(string name, string email)
+        {
+            return name + email;
+        }
+    }
+}
---
+++ Banco/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using Course.Entities;
+using System;
+using System.Globalization;
+using System.Security.Cryptography;
+
+
+
+namespace Course
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> ListAccounts = new List<Account>();
+            Random random = new Random();
+            string path = @"C:\\Users\\art0063186\\Desktop\\Projetos c#\\Banco do AlyShow";
+           
+            Console.WriteLine("------------------------");
+            Console.WriteLine("----Banco do AlyShow----");
+            Console.WriteLine("------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Login:");
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                string sourceFolderPath = Path.GetDirectoryName(path);
+                string targetFolderPath = sourceFolderPath + @"\Banco do Brasil\Contas.txt";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFolderPath))
+                {
+                    foreach (string line in lines)
+                    {
+
+                        int number = random.Next(1000, 9999);
+                        Console.WriteLine("Nome: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Email: ");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Senha: ");
+                        string adress = Console.ReadLine();
+
+                        Account account = new Account(name, email, adress, number);
+                        ListAccounts.Add(account);
+
+                        sw.WriteLine("Nome: " + account.Name);
+                        sw.WriteLine("Número: " + account.Number);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
